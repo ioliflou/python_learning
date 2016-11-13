82393065fadc7da7f4dece7ad8bbbf0a63d174e4
Dmytro Kurbakov <kurbakov@users.noreply.github.com>
2016-11-13 21:54:23 +0100

    Create template.py

---
+++ twitter/template.py
@@ -0,0 +1,38 @@
+# this is a template that you can use
+# anyway feel free to change whatever you want
+import json
+
+def read_file(path_to_file):
+    # step 1: read file
+
+    result = str()
+
+    # write the code to read data from file
+    # and save it the variable result
+    return result
+
+
+def make_dict(string_of_the_file):
+    # step 2: create the dictionary
+
+    result = dict()
+
+    # make the function to create the dictionary
+    # base on some string as the input
+    return result
+
+
+def print_value(string_key, dictionary_variable):
+    # step 3: print the value of the key from input
+    result = str()
+
+    # write the function to rint the value based on the input
+    # key and dictionary
+    # if the key is not in the dictionary:
+    # return string "NULL"
+
+    return result
+
+
+if __name__ == "__main__":
+    print "this part will be automatically executed"
